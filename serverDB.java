b5ff1aeb3ec9353dff14907a195faa92983ad72d
vasanthsrinu <vasanthnarsing96@gmail.com>
2020-05-28 07:21:15 +0000

    Deleting

--- serverDB.java
+++
@@ -1,3 +0,0 @@
-This is the new file to write a code
-This line is to check whether we are bale to push the code while the changes are being made in the remote repo
-
--- serverutils.java
+++
@@ -1,2 +0,0 @@
-This is the new file to write a code
-Modifyinh the file to pull the request from remote to local
